07d214cb058cead6b83bbaf2a9c69f73fe9fa3d7
devbsu <devbsu@naver.com>
2025-11-10 15:53:12 +0900

    fix: router 오류 수정

--- src/router/index.js
+++ src/router/index.js
@@ -1,46 +1,43 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import interviewRouting from "@/router/interviewRouting"
-import trendRouting from "@/router/trendRouting"
-import learningRouting from './learningRouting'
-import myRouting from "@/router/myRouting"
-import resumeRouting from './resumeRouting'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import interviewRouting from "@/router/interviewRouting";
+import trendRouting from "@/router/trendRouting";
+import learningRouting from "./learningRouting";
+import myRouting from "@/router/myRouting";
+import resumeRouting from "./resumeRouting";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "home",
+    component: HomeView,
   },
   {
-    path: '/about',
-    name: 'about',
+    path: "/login",
+    name: "login",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-
-    component: () => import(/* webpackChunkName: "login" */ '../views/LoginView.vue')
+    component: () => import(/* webpackChunkName: "login" */ "../views/LoginView.vue"),
   },
   {
-    path: '/signup',
-    name: 'signup',
+    path: "/signup",
+    name: "signup",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/SignupView.vue')
+    component: () => import(/* webpackChunkName: "signup" */ "../views/SignupView.vue"),
   },
   ...interviewRouting,
   ...trendRouting,
   ...myRouting,
   ...learningRouting,
-  ...resumeRouting
-
-
-]
+  ...resumeRouting,
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
 export default router;
