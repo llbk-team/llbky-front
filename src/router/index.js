3c15fce15120da4e7dfa63b2f7e09ba88046caba
bale0610 <see23069@naver.com>
2025-11-10 15:17:28 +0900

    fix: index.js 해결?

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import interviewRouting from "@/router/interviewRouting"
+import trendRouting from "@/router/trendRouting"
+
 
 const routes = [
   {
@@ -21,8 +24,10 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    component: () => import(/* webpackChunkName: "about" */ '../views/SignupView.vue')
+  },
+  ...interviewRouting,
+  ...trendRouting
 ]
 
 const router = createRouter({
