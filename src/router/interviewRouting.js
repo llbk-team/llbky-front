92370411e291c47158af905effcef4c04d65c50a
kyum22n <aphrod207@naver.com>
2025-11-10 14:58:00 +0900

    feat: 면접 뷰&라우터 추가

---
+++ src/router/interviewRouting.js
@@ -0,0 +1,24 @@
+const routes = [
+  {
+    path: "/interview/report",
+    name: "Report",
+    component: () => import(/*webpackChunkName: "interviewRouting"*/ "@/views/interview/Report.vue")
+  },
+  {
+    path: "/interview/report/:id",
+    name: "ReportDetail",
+    component: () => import(/*webpackChunkName: "interviewRouting"*/ "@/views/interview/ReportDetail.vue")
+  },
+  {
+    path: "/interview/mock",
+    name: "Mock",
+    component: () => import(/*webpackChunkName: "interviewRouting"*/ "@/views/interview/Mock.vue")
+  },
+  {
+    path: "/interview/progress",
+    name: "Progress",
+    component: () => import(/*webpackChunkName: "interviewRouting"*/ "@/views/interview/Progress.vue")
+  }
+];
+
+export default routes;
