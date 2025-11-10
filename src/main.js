844545a83ced7c5b966139be8191b76a99b479e6
kyum22n <aphrod207@naver.com>
2025-11-10 13:48:43 +0900

    feat: 아이콘 추가

--- src/main.js
+++ src/main.js
@@ -3,4 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// remixicon 
+// npm install remixicon로 추가해주세요!
+import "remixicon/fonts/remixicon.css";
+
+// bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+
+
 createApp(App).use(store).use(router).mount('#app')
